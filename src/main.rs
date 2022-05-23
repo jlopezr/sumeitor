363a42a015b9f782fca33e7ee4d4b471cd266d2c
Juan López Rubio <j.lopez.r@gmail.com>
2022-05-23 20:32:23 +0200

    Custom numbers input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
-use std::time::Instant;
+use crossterm::cursor::MoveToNextLine;
 use rand::Rng;
+use std::time::Instant;
+
+use std::io::stdout;
+
+use crossterm::{
+    cursor::{MoveLeft, MoveRight},
+    event::{read, Event, KeyCode},
+    execute,
+    style::Print,
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
 
 #[macro_use]
 extern crate text_io;
@@ -11,7 +22,7 @@ enum OpType {
     Sub,
 }
 
-impl OpType { 
+impl OpType {
     fn to_str(self) -> &'static str {
         match self {
             OpType::Add => "+",
@@ -21,31 +32,79 @@ impl OpType {
 
     fn do_op(self: Self, a: i32, b: i32) -> i32 {
         match self {
-            OpType::Add => a+b,
-            OpType::Sub => a-b,
+            OpType::Add => a + b,
+            OpType::Sub => a - b,
         }
     }
-} 
+}
 
-fn old_do_input() -> i32 {    
+fn old_do_input() -> i32 {
     read!()
 }
 
-fn do_input() -> i32 {    
-    read!()
+fn do_input() -> i32 {
+    let mut input = String::new();
+
+    execute!(
+        stdout(),
+        //SetForegroundColor(Color::Blue),
+        //SetBackgroundColor(Color::Red),
+        MoveRight(3),
+    )
+    .ok();
+
+    enable_raw_mode();
+
+    loop {
+        let event = read().unwrap();
+
+        if event == Event::Key(KeyCode::Esc.into()) {
+            disable_raw_mode();
+            std::process::exit(0);
+        }
+
+        if event == Event::Key(KeyCode::Backspace.into()) {
+            if input.len() > 0 {
+                input.remove(0);
+                execute!(stdout(), Print(" ")).ok();
+            }
+        }
+
+        if event == Event::Key(KeyCode::Enter.into()) {
+            execute!(stdout(), MoveToNextLine(1)).ok();
+            println!();
+            break;
+        }
+
+        match event {
+            Event::Key(c) => {
+                if c.code >= KeyCode::Char('0') && c.code <= KeyCode::Char('9') {
+                    match c.code {
+                        KeyCode::Char(ch) => {
+                            input.insert(0, ch);
+                            execute!(stdout(), Print(ch), MoveLeft(2),).ok();
+                        }
+                        _ => (),
+                    }
+                }
+            }
+            _ => (),
+        }
+    }
+
+    disable_raw_mode();
+    input.parse().unwrap()
 }
 
 fn main() {
-
     let mut good = 0;
     let mut bad = 0;
     let mut input: i32;
     let start = Instant::now();
     let mut rng = rand::thread_rng();
     let mut end = false;
-    
+
     while !end {
-    
         let a = rng.gen_range(0..99);
         let b = rng.gen_range(0..10);
         let op: OpType = OpType::Add;
@@ -54,24 +113,25 @@ fn main() {
         println!("  {:>2}", a);
         println!("{} {:>2}", op.to_str(), b);
         println!("------");
-        
+
         input = do_input();
-        
+
         if input == op.do_op(a, b) {
-            good = good+1;
+            println!("OK");
+            good = good + 1;
         } else {
-            bad = bad+1;
+            println!("KO");
+            bad = bad + 1;
         }
 
-        if start.elapsed().as_secs() > 60*1 {
+        if start.elapsed().as_secs() > 60 * 1 {
             end = true;
             println!();
             println!("Respuestas:");
-            println!("Totales:   {}", good+bad);
+            println!("Totales:   {}", good + bad);
             println!("Correctas: {}", good);
 
             //TODO Guardar log en archivo
         }
     }
-
 }
